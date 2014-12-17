abcbdff49c9c33cf6136667bc1371d192e1cb1a6
Francisco De La Cruz <helloquico@gmail.com>
2014-12-17 18:54:55 -0500

    Recap of Ember flow

--- js/app.js
+++ js/app.js
@@ -2,15 +2,17 @@ App = Ember.Application.create({
   // Basic logging, e.g. "Transitioned into 'post'"
   LOG_TRANSITIONS: true
 });
-
+// 1. Define routes
 App.Router.map(function() {
   this.route('first-route', { path: 'first-route-in-url' })
 });
-
+// 2. Load route and models
 App.FirstRouteRoute = Ember.Route.extend({
-  
+  model: function() {
+    return ['first item', 'second item']
+  }
 });
-
+// 3. Models (if any) get passed to the controller
 App.FirstRouteController = Ember.Controller.extend({
   actions: {
     headerclicked: function () {
