6aa41139ddfbbab5c7ddec532b0c5c27b2e6a26f
Francisco De La Cruz <helloquico@gmail.com>
2014-12-18 02:24:05 -0500

    Ember getters and setters

--- js/app.js
+++ js/app.js
@@ -4,17 +4,28 @@ App = Ember.Application.create({
 });
 
 App.Router.map(function() {
-  this.route('about');
-});
-
-App.AboutRoute = Ember.Route.extend({
-  renderTemplate: function () {
-    this.render('aboutme');
-  }
+  // put your routes here
 });
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
     return ['red', 'yellow', 'blue'];
+  }
+});
+
+App.IndexController = Ember.ArrayController.extend({
+  firstName: 'Johnny',
+  lastName: 'Depp',
+  actions: {
+    getActor: function () {
+      var actor = this.get('firstName') + ' ' + this.get('lastName');
+      alert('The actor is ' + actor);
+    },
+    setActor: function () {
+      this.set('firstName', 'Michael');
+      this.set('lastName', 'Schofield');
+      var newActor = this.get('firstName') + ' ' + this.get('lastName');
+      alert('The new actor is ' + newActor);
+    }
   }
 });
