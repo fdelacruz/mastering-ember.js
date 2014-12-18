640e2c70417d32798039ba9f47f814bf22138bd8
Francisco De La Cruz <helloquico@gmail.com>
2014-12-18 01:55:33 -0500

    How to render a different template from the default

--- js/app.js
+++ js/app.js
@@ -7,6 +7,12 @@ App.Router.map(function() {
   this.route('about');
 });
 
+App.AboutRoute = Ember.Route.extend({
+  renderTemplate: function () {
+    this.render('aboutme');
+  }
+});
+
 App.IndexRoute = Ember.Route.extend({
   model: function() {
     return ['red', 'yellow', 'blue'];
