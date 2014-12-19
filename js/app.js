52a3fc6e6ea2b06a2ce77b55c3cf0d08a0a4591f
Francisco De La Cruz <helloquico@gmail.com>
2014-12-18 21:17:54 -0500

    Defining, creating and using models

--- js/app.js
+++ js/app.js
@@ -7,17 +7,31 @@ App.Router.map(function() {
   // put your routes here
 });
 
+App.PersonAdapter = DS.FixtureAdapter.extend({});
+
+App.Person = DS.Model.extend({
+  firstName: DS.attr(),
+  lastName: DS.attr(),
+  age: DS.attr()
+});
+
 App.IndexRoute = Ember.Route.extend({
   model: function() {
-    return ['red', 'yellow', 'blue'];
+    return this.store.find('person');
   }
 });
 
-App.IndexController = Ember.ArrayController.extend({
-  firstName: 'Christina',
-  actions: {
-    addNewItem: function() {
-      this.get('model').pushObject('new item');
-    }
+App.Person.FIXTURES = [
+  { 
+    id: 1,
+    firstName: 'John',
+    lastName: 'Cena',
+    age: 27
+  },
+  {
+    id: 2,
+    firstName: 'Joan',
+    lastName: 'Kelly',
+    age: 26
   }
-});
+]
