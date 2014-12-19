4a29f298341ab3037187d81e02d77cb295037377
Francisco De La Cruz <helloquico@gmail.com>
2014-12-19 00:19:05 -0500

    Ember REST and Fixture Adapters

--- js/app.js
+++ js/app.js
@@ -7,31 +7,29 @@ App.Router.map(function() {
   // put your routes here
 });
 
-App.PersonAdapter = DS.FixtureAdapter.extend({});
+App.ApplicationAdapter = DS.RESTAdapter.extend({});
 
 App.Person = DS.Model.extend({
-  firstName: DS.attr(),
-  lastName: DS.attr(),
-  age: DS.attr()
+  firstName: DS.attr()
 });
+
+App.Car = DS.Model.extend({
+  type: DS.attr()
+});
+
+App.Person.FIXTURES = [
+  {
+    id: 1,
+    firstName: 'Pablo'
+  }
+]
+
+App.PersonAdapter = DS.FixtureAdapter.extend({});
+
+App.CarAdapter = DS.RESTAdapter.extend({});
 
 App.IndexRoute = Ember.Route.extend({
   model: function() {
     return this.store.find('person');
   }
 });
-
-App.Person.FIXTURES = [
-  { 
-    id: 1,
-    firstName: 'John',
-    lastName: 'Cena',
-    age: 27
-  },
-  {
-    id: 2,
-    firstName: 'Joan',
-    lastName: 'Kelly',
-    age: 26
-  }
-]
