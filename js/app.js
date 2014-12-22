6a25df7f265d95442793535630308735ab0b3204
Francisco De La Cruz <helloquico@gmail.com>
2014-12-22 18:35:17 -0500

    Nested Routes

--- js/app.js
+++ js/app.js
@@ -4,37 +4,9 @@ App = Ember.Application.create({
 });
 
 App.Router.map(function() {
-  this.route("about", { path: "/about" });
-});
-
-App.IndexRoute = Ember.Route.extend({
-  model: function(){
-    return ['red', 'yellow', 'blue']
-  }
-});
-
-App.IndexController = Ember.ArrayController.extend({
-  needs: ['about'],
-
-  actions: {
-    callAboutController: function(){
-      this.get('controllers.about').send('popup');
-    },
-    popup: function () {
-      alert('Popup from index controller');
-    }
-  }
-});
-
-App.AboutController = Ember.ObjectController.extend({
-  needs: ['index'],
-
-  actions: {
-    callIndexController: function(){
-      this.get('controllers.index').send('popup');
-    },
-    popup: function () {
-      alert('Popup from about controller');
-    }
-  }
+  this.resource('articles', { path: 'articles' }, function(){
+    this.route('index', { path: '/' });
+    this.route('new',   { path: '/new' });
+    this.route('edit',  { path: '/edit/:id' });
+  });
 });
