991f326a7205a5912e3e39a3fdd2219cbea67c93
Francisco De La Cruz <helloquico@gmail.com>
2014-12-22 00:13:57 -0500

    Dealing with form events

--- js/app.js
+++ js/app.js
@@ -3,52 +3,21 @@ App = Ember.Application.create({
   LOG_TRANSITIONS: true
 });
 
-Ember.Inflector.inflector.irregular('person', 'people');
-
 App.Router.map(function() {
-  this.route('people', { path: '/people' });
-  this.route('person', { path: '/people/:person_id' });
+  // put your routes here
 });
 
-App.PeopleRoute = Ember.Route.extend({
+App.IndexController = Ember.ObjectController.extend({
+  actions: {
+    handleSubmit: function(){
+      alert('The firstName is ' + this.get('firstName'));
+    }
+  }
+});
+
+App.IndexRoute = Ember.Route.extend({
   model: function(){
-    return this.store.find('person');
+    return ['red', 'yellow', 'blue']
   }
 });
 
-// Route not needed since I used :person_id in the route definition above
-// App.PersonRoute = Ember.Route.extend({
-//   model: function(params){
-//     return this.store.find('person', params.person_id)
-//   }
-// });
-
-App.Person = DS.Model.extend({
-  firstName: DS.attr(),
-  lastName: DS.attr(),
-});
-
-App.PersonAdapter = DS.FixtureAdapter.extend();
-
-App.Person.FIXTURES = [
-  {
-    id: 1,
-    firstName: 'Ryan',
-    lastName: 'Giggs'
-  },
-  {
-    id: 2,
-    firstName: 'Tom',
-    lastName: 'Dale'
-  },
-  {
-    id: 3,
-    firstName: 'Yehuda',
-    lastName: 'Katz'
-  },
-  {
-    id: 4,
-    firstName: 'Erik',
-    lastName: 'Bryn'
-  }
-];
