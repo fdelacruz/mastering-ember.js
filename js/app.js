fd2a2e6e25f7cc4f9867fb4e6c042d236be601ab
Francisco De La Cruz <helloquico@gmail.com>
2014-12-22 02:55:08 -0500

    Decorating Controllers

--- js/app.js
+++ js/app.js
@@ -7,17 +7,13 @@ App.Router.map(function() {
   // put your routes here
 });
 
-App.IndexController = Ember.ObjectController.extend({
-  actions: {
-    handleSubmit: function(){
-      alert('The firstName is ' + this.get('firstName'));
-    }
-  }
-});
-
 App.IndexRoute = Ember.Route.extend({
   model: function(){
     return ['red', 'yellow', 'blue']
+  },
+  setupController: function(controller, model){
+    controller.set('model', model);
+    controller.set('owner', 'Tom Dale');
   }
 });
 
